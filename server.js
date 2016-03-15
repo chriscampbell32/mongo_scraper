43f905ec8748ea904d90ced75593718fe906334d
chriscampbell32 <chriscampbell32@yahoo.com>
2016-03-14 20:16:56 -0400

    app is running

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
 var request = require('request');
 var expressHandlebars = require('express-handlebars');
+var PORT = process.env.PORT || 8080;
+
+
+
+
+
+
+app.listen(PORT, function(req, res){
+    console.log('app running on port %s', PORT);
+});
