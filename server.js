34800902e0325d8c9940af10b0978cb9489248b0
chriscampbell32 <chriscampbell32@yahoo.com>
2016-03-14 20:29:04 -0400

    models added, setting up server.js file

--- server.js
+++ server.js
@@ -5,7 +5,28 @@ var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
 var request = require('request');
 var expressHandlebars = require('express-handlebars');
+
+
 var PORT = process.env.PORT || 8080;
+
+//handlebars setup
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//logger
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+///////CONNECT TO THE DATABASE//////
+
+
+
+
+//require schema
+var Note = require('./models/noteModel.js');
+var Article = require('./models/articleModel.js');
 
 
 
